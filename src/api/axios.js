085869374eb178ee84f942b3662e8bba4cd10fd2
BoteritoROOT <d.sajor2003rp@gmail.com>
2024-10-25 16:43:16 -0400

    sfs

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 const instance = axios.create({
      baseURL : 'https://backend-si1-production-0e10.up.railway.app/api',
-    withCredentials: true
+    withCredentials: true,
+    timeout: 5000 
 })
 
 export default instance

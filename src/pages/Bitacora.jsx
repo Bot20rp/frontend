fe344bd5c61ea35dfa4707944524ed47c916c080
BoteritoROOT <d.sajor2003rp@gmail.com>
2024-10-25 19:49:02 -0400

    bitacora

--- src/pages/Bitacora.jsx
+++ src/pages/Bitacora.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './bitacora.css';
-import { useAuth } from '../context/AuthContext';  
+import { bitacoraa } from '../api/auth';  
 export const Bitacora = () => {
-  const {bitacoraa} = useAuth();
   const [bitacoras, setBitacoras] = useState([]); // Estado para almacenar los registros de la bitácora
   const [loading, setLoading] = useState(true); // Estado para manejar el loading
 

dbc162a4a058a9486d50f24cc9ce999a0322f4d3
BoteritoROOT <d.sajor2003rp@gmail.com>
2024-10-27 23:22:22 -0400

    probando

--- src/api/auth.js
+++ src/api/auth.js
@@ -4,101 +4,112 @@ import instance from "./axios"
 
 const tiempoEspera = 10000;
 
-export const registerRequest = user => instance.post(`/clientReg`,user) 
-export const loginRequest = user => instance.post(`/login`,user ,{
+export const registerRequest = user => instance.post(`/clientReg`, user)
+export const loginRequest = user => instance.post(`/login`, user, {
     withCredentials: true
 });
 
-export const logoutRequest= ()=>instance.post('/logout')
-export const obtenerRequest = () => { return instance.get('/obtener',{
-    withCredentials: true,
-    timeout: tiempoEspera 
-})};
+export const logoutRequest = () => instance.post('/logout')
+export const obtenerRequest = () => {
+    return instance.get('/obtener', {
+        withCredentials: true,
+        timeout: tiempoEspera
+    })
+};
 export const verityTokenResquest = (token) => instance.get('/verify', {
     headers: {
         Authorization: `Bearer ${token}`
     }
 });
-export const actualizarUsuario = user => instance.patch(`/usuario/actualizar`,{data :user ,
+export const actualizarUsuario = user => instance.patch(`/usuario/actualizar`, {
+    data: user,
     headers: {
-        'Content-Type': 'application/json', 
+        'Content-Type': 'application/json',
     },
 });
-export const eliminarUsuario = user => instance.delete('/usuario/del',{ data : user,
+export const eliminarUsuario = user => instance.delete('/usuario/del', {
+    data: user,
     headers: {
-        'Content-Type': 'application/json', 
+        'Content-Type': 'application/json',
     },
 })
 
 
 // GESTION PROVEEDOR
 
-export const registrarProveedorRequest = user => instance.post(`/proveedor`,user);
-export const extraerID = user => instance.post(`/proveedor/ex`,user);
-export const eliminarProveedor = user => instance.delete(`/proveedor/delete`,{ data : user, 
+export const registrarProveedorRequest = user => instance.post(`/proveedor`, user);
+export const extraerID = user => instance.post(`/proveedor/ex`, user);
+export const eliminarProveedor = user => instance.delete(`/proveedor/delete`, {
+    data: user,
     headers: {
-        'Content-Type': 'application/json', 
+        'Content-Type': 'application/json',
     },
 });
-export const actualizarProveedores = user => instance.patch(`/proveedor/update`,{data :user ,
+export const actualizarProveedores = user => instance.patch(`/proveedor/update`, {
+    data: user,
     headers: {
-        'Content-Type': 'application/json', 
+        'Content-Type': 'application/json',
     },
 });
-export const obtenerRequestProveedor = () => {return instance.get(`/proveedor`,{withCredentials: true})};
+export const obtenerRequestProveedor = () => { return instance.get(`/proveedor`, { withCredentials: true }) };
 
 
 //Gestion Empleado
 
-export const registerEmpleado = user => instance.post(`/empleadoreg`,user)
+export const registerEmpleado = user => instance.post(`/empleadoreg`, user)
 
 //Gestion Categoria
 
-export const insertarCategoriaPadre = user => instance.post(`/categoriaPadre`,{ data: user,
-    headers:{
-        'Content-Type': 'application/json', 
+export const insertarCategoriaPadre = user => instance.post(`/categoriaPadre`, {
+    data: user,
+    headers: {
+        'Content-Type': 'application/json',
     }
 })
-export const insertarCategoriaHija = user => instance.post(`/categoriaHija`,{ data: user,
-    headers:{
-        'Content-Type': 'application/json', 
+export const insertarCategoriaHija = user => instance.post(`/categoriaHija`, {
+    data: user,
+    headers: {
+        'Content-Type': 'application/json',
     }
 })
-export const actualizarCategoria = user => instance.patch('/catmodificar',user);
-export const obtenerCategorias = () => {return instance.get(`/getCategoria`);};
-export const eliminarCategorias = user => instance.delete(`/DeleteCategoria`,{data : user,
-    headers :{
-        'Content-Type': 'application/json', 
+export const actualizarCategoria = user => instance.patch('/catmodificar', user);
+export const obtenerCategorias = () => { return instance.get(`/getCategoria`); };
+export const eliminarCategorias = user => instance.delete(`/DeleteCategoria`, {
+    data: user,
+    headers: {
+        'Content-Type': 'application/json',
     }
 });
 
 
 //GESTION PRODUCTO
-export const insertarProducto = user => instance.post(`/productoReg`, { data: user ,
-    headers:{
-        'Content-Type': 'application/json', 
+export const insertarProducto = user => instance.post(`/productoReg`, {
+    data: user,
+    headers: {
+        'Content-Type': 'application/json',
     },
 });
 
 
 
-export const obtenerProductos = () => {return instance.get(`/producto`)};
+export const obtenerProductos = () => { return instance.get(`/producto`) };
 
-export const actualizarProducto = user => instance.patch(`/producto/actualizar`,{data :user ,
-    headers:{
-        'Content-Type': 'application/json', 
+export const actualizarProducto = user => instance.patch(`/producto/actualizar`, {
+    data: user,
+    headers: {
+        'Content-Type': 'application/json',
     },
 });
 
 
 // BITACORA 
 
-export const bitacoraa = () => {return instance.get(`/bitacora`)}; 
+export const bitacoraa = () => { return instance.get(`/bitacora`) };
 
 
 //PRIVILEGIOS O PERMISO
 
-export const permisos = () => {return instance.get(`/permisos`)};
+export const permisos = () => { return instance.get(`/permisos`) };
 
 export const actualizarPermisos = (privilegios, rolSeleccionado) => instance.patch(`/updaPermisos2`, {
     data: {                    // Enviando ambos como un objeto dentro de "data"
@@ -114,4 +125,9 @@ export const actualizarPermisos = (privilegios, rolSeleccionado) => instance.pat
 
 //COMBOS!!!!
 
-export const insertarCombo = (data) => instance.post(`/combos`,data);
+export const insertarCombo = (data) => instance.post(`/combos`, {
+    data,
+    headers: {
+        'Content-Type': 'application/json',
+    }
+});

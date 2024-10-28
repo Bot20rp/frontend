eb52d90a9684d35a05ac84d86db1431761bc5dfd
BoteritoROOT <d.sajor2003rp@gmail.com>
2024-10-28 17:40:31 -0400

    probando

--- src/ProtectPrivilegios.jsx
+++ src/ProtectPrivilegios.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
 
 const ProtectedRoutePrivilegios = ({ privilegeDescription, children }) => {
   const { user } = useAuth(); // Mover la llamada dentro del componente
-
+  console.log(user)
   const hasPrivilege = user?.permisos.some(
     (permiso) => permiso.Descripcion === privilegeDescription && permiso.Estado === 1
   );

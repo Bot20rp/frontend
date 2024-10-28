b4bc4453a8653e394f7b7e8ccf463fd5c2a01ead
BoteritoROOT <d.sajor2003rp@gmail.com>
2024-10-28 17:56:07 -0400

    jajaj si da

--- src/ProtectPrivilegios.jsx
+++ src/ProtectPrivilegios.jsx
@@ -3,8 +3,7 @@ import { useAuth } from './context/AuthContext';
 
 const ProtectedRoutePrivilegios = ({ privilegeDescription, children }) => {
   const { user } = useAuth();
-  console.log(user)
-  console.log(user.user.permisos)
+  console.log(user.user.rol)
   const hasPrivilege = user?.user.permisos.some(
     (permiso) => permiso.Descripcion === privilegeDescription && permiso.Estado === 1
   );

5fcbdc42def5d6ee0941d9e56d5f6ed6a231c050
BoteritoROOT <d.sajor2003rp@gmail.com>
2024-10-27 22:55:54 -0400

    compras

--- src/pages/PaqueteCompra/Compras.jsx
+++ src/pages/PaqueteCompra/Compras.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
 import '../../css/AdmiCompraCss/compras.css';
 import { useAuth } from '../../context/AuthContext';
 

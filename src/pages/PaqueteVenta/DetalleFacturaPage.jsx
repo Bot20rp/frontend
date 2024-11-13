59a228fa53067e78a59622d9da7a6b87d4051b67
BoteritoROOT <d.sajor2003rp@gmail.com>
2024-11-13 00:45:09 -0400

    go go

--- src/pages/PaqueteVenta/DetalleFacturaPage.jsx
+++ src/pages/PaqueteVenta/DetalleFacturaPage.jsx
@@ -2,8 +2,44 @@ import React from 'react'
 
 function DetalleFacturaPage() {
   return (
-    <div>
-      aqui anulacion factura
+    <div className='comprobantes'>
+      <h1>Detalle de Facturas</h1>
+      <h4>Nro Factura</h4>
+
+      <div>
+        <input type="text" placeholder='nro factura' />
+      </div>
+
+      <div className='gestion'>
+        <div id='xxiv'>
+          <h4>Desde</h4>
+          <input type="date" />
+        </div>
+        <div id='xxiv'>
+          <h4>Hasta</h4>
+          <input type="date" />
+        </div>
+      </div>
+
+      <button>Listar</button>
+
+      <h1>Facturas</h1>
+      
+      <div className='tablaComprobantes'>
+        <table>
+          <thead>
+            <tr>
+              <th>Tipo Venta</th>
+              <th>Fecha</th>
+              <th>Comprobante</th>
+              <th>Cliente</th>
+              <th>Monto Total</th>
+              <th>Ver/Imprimir</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+
     </div>
   )
 }

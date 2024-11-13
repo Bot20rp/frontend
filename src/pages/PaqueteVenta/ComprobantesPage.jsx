28d3f2bb750dd94b92e43df7dc159e47ac846def
BoteritoROOT <d.sajor2003rp@gmail.com>
2024-11-13 00:15:28 -0400

    comprobantes

--- src/pages/PaqueteVenta/ComprobantesPage.jsx
+++ src/pages/PaqueteVenta/ComprobantesPage.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import '../../css/AdmiVentaCss/ComprobantesPage.css'
 
 function ComprobantesPage() {
   return (
-    <div>
-      soy comprobante
+    <div className='comprobantes'>
+      <h1>Listado de Comprobantes</h1>
+      <h4>Cliente</h4>
+
+      <div>
+        <input type="text" placeholder='NIT' />
+      </div>
+
+      <div className='gestion'>
+        <div id='xxiv'>
+          <h4>Desde</h4>
+          <input type="date" />
+        </div>
+        <div id='xxiv'>
+          <h4>Hasta</h4>
+          <input type="date" />
+        </div>
+      </div>
+
+      <h1>Comprobantes</h1>
+      
+      <div className='tablaComprobantes'>
+        <table>
+          <thead>
+            <tr>
+              <th>Tipo Venta</th>
+              <th>Fecha</th>
+              <th>Comprobante</th>
+              <th>Cliente</th>
+              <th>Monto Total</th>
+              <th>Ver/Imprimir</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+
     </div>
   )
 }

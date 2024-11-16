7d6aff219ad585eea31bc72fea877be3f71c2728
BoteritoROOT <d.sajor2003rp@gmail.com>
2024-11-15 20:46:30 -0400

    perfil user

--- src/components/Perfi/Perfil.jsx
+++ src/components/Perfi/Perfil.jsx
@@ -7,6 +7,8 @@ import './perfil.css';
 export const Perfil = () => {
     const { logout, user } = useAuth();
     const { purchaseHistory } = useContext(CartContext);
+    const pedidos = ["Pedido 1", "Pedido 2", "Pedido 3"]; // Lista de pedidos
+
     const navigate = useNavigate();
 
     const handleLogout = async () => {
@@ -21,34 +23,86 @@ export const Perfil = () => {
     return (
         <div className="profile">
             <h2>Perfil del Usuario</h2>
-            {user ? (
-                <>
-                    <p>
-                        <strong>Rol: {user.user.rol}</strong>
-                    </p>
-                    <p>
-                        <strong>Correo: {user.user.email}</strong>
-                    </p>
 
-                    <h3>Historial de Compras</h3>
-                    <ul>
-                        {purchaseHistory.length > 0 ? (
-                            purchaseHistory.map((compra, index) => (
-                                <li key={index}>
-                                    <p>Producto: {compra.productName}</p>
-                                    <p>Precio: Bs {compra.price}</p>
-                                    <p>Fecha: {compra.date}</p>
-                                    <hr />
-                                </li>
-                            ))
-                        ) : (
-                            <p>No has realizado ninguna compra aún.</p>
-                        )}
-                    </ul>
-                </>
-            ) : (
-                <p>Cargando información del usuario...</p>
-            )}
+            <div>
+                <h1>JOAN DANIEL</h1>
+            </div>
+
+            <div id="datosPersonales">
+
+                <h3>Datos Personales</h3>
+                <div className="personaje">
+
+                    <div id="datos">
+                        <div id="minis1">
+                            <h4>CI</h4>
+                            <h4>98745128</h4>
+                        </div>
+                        <div id="minis1">
+                            <h4>DIRECCION</h4>
+                            <h4>98745128</h4>
+                        </div>
+                        <div id="minis1">
+                            <h4>FECHA NACIMIENTO</h4>
+                            <h4>98745128</h4>
+                        </div>
+                        <div id="minis1">
+                            <h4>CORREO</h4>
+                            <h4>98745128</h4>
+                        </div>
+                    </div>
+
+                    <div id="datos">
+                        <div id="minis1">
+                            <h4>NIT</h4>
+                            <h4>98745128</h4>
+                        </div>
+                        <div id="minis1">
+                            <h4>TELEFONO</h4>
+                            <h4>98745128</h4>
+                        </div>
+                        <div id="minis1">
+                            <h4>GENERO</h4>
+                            <h4>98745128</h4>
+                        </div>
+                        <div id="minis1">
+                            <h4>CONTRASEÑA</h4>
+                            <h4>98745128</h4>
+                        </div>
+                    </div>
+                </div>
+
+                <button>EditarPerfil</button>
+
+            </div>
+
+            <div id="pedidos">
+                <h2>Mis Pedidos</h2>
+                <div className="soyPedidos">
+                    {pedidos.map((pedido, index) => (
+                        <div key={index} id="numeroP">
+                            {pedido}
+                            <h4>Fecha : 12/12/2024</h4>
+                            <h4>Total : 12/12/2024</h4>
+                            <h4>Estado : 12/12/2024</h4>
+                        </div>
+                    ))}
+                </div>
+                <a href="">Consultar Empresa</a>
+            </div>
+
+            <div id="historial">
+                <h2>Historial de mis Pedidos</h2>
+                <button>listar mis Comprobantes</button>
+                <table>
+                    <thead>
+                        <td>Fecha</td>
+                        <td>Monto Total</td>
+                        <td>Ver/Imprimir</td>
+                    </thead>
+                </table>
+            </div>
+
 
             <button onClick={handleLogout}>Cerrar sesión</button>
         </div>
